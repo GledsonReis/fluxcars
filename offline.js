51a7855aa8d2d7b5424819708c9bca01fcf502e9
GledsonReis <gledsonthegreeder@gmail.com>
2022-11-06 17:21:47 -0300

    first commit

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1667762781,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite3-sheet0.png",
+		"images/carro1-sheet0.png",
+		"images/gameover-sheet0.png",
+		"images/block1-sheet0.png",
+		"images/block1-sheet1.png",
+		"images/carro2-sheet0.png",
+		"images/scoresensor-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"media/237375__squareal__car-crash.m4a",
+		"media/237375__squareal__car-crash.ogg",
+		"media/gameover - iwan gabovitch - dark ambience loop.ogg",
+		"media/juhani junkala [chiptune adventures] 1. stage 1.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
